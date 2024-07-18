0b0f7bee499196b11df93313d029bd0e0cfa12c2
Mo Beigi <me@mobeigi.com>
2024-07-18 16:53:46 +1000

    Add more structure for button. Comment out CSS for button/a for now for revisiting

--- app/src/App/Panel/index.tsx
+++ app/src/App/Panel/index.tsx
@@ -1,18 +1,15 @@
-import { DescriptionWrapper } from './styled'
+import { PanelContainer, DescriptionWrapper } from './styled'
 
 const Panel = () => {
   return (
-    <>
+    <PanelContainer>
       <DescriptionWrapper>
         <p><strong>Puzzle: </strong> TODO</p>
         <p><strong>Difficulty: </strong> Hidden</p>
       </DescriptionWrapper>
-      {/* <p>CARD 1</p>
-      <p>CARD 2</p>
-      <p>CARD 3</p>
-      <p>CARD 4</p>
-      <button>Submit</button> */}
-    </>
+
+      <button>Submit</button> 
+    </PanelContainer>
   )
 }
 
--- app/src/App/Panel/styled.ts
+++ app/src/App/Panel/styled.ts
@@ -1,14 +1,24 @@
 import { styled } from 'styled-components'
 
-export const DescriptionWrapper = styled.main`
-    display: flex;
-    flex-direction: column;
-    padding: 0;
-    margin: 0;
-    gap: 0.2em;
+export const PanelContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 100%;
+  align-items: center;
+  justify-content: center;
+	gap: 1em;
+`
 
-    p {
-        margin: 0;
-        padding: 0;
-    }
+export const DescriptionWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+	margin: 0;
+  gap: 0.2em;
+
+	p {
+		margin: 0;
+		padding: 0;
+	}
 `
