7ae626d0580de0b835802356b96baaf4b7872524
Mo Beigi <me@mobeigi.com>
2024-07-18 19:01:20 +1000

    Tweak chess board colors

--- app/src/App/ChessBoard/index.tsx
+++ app/src/App/ChessBoard/index.tsx
@@ -1,5 +1,16 @@
 import { Chessboard } from "react-chessboard";
 
-const ChessBoard = () => <Chessboard position="2kr3r/pp4p1/2pb2p1/4qp2/3p2P1/3Q3P/PP2PBB1/R2R2K1 w - - 0 21" arePiecesDraggable={false} />
+const customDarkSquareColor = "#b58b69";
+const customLightSquareColor = "#f1dec2";
+const customArrowColor = "#0f8ca8";
+
+const ChessBoard = () => <Chessboard 
+    position="2kr3r/pp4p1/2pb2p1/4qp2/3p2P1/3Q3P/PP2PBB1/R2R2K1 w - - 0 21" 
+    arePiecesDraggable={false} 
+    customDarkSquareStyle={{ backgroundColor: customDarkSquareColor }}
+    customLightSquareStyle={{ backgroundColor: customLightSquareColor }}
+    customBoardStyle={{ boxShadow: '0 5px 15px rgba(0, 0, 0, 0.5 )'}}
+    customArrowColor={customArrowColor}
+/>
 
 export default ChessBoard
