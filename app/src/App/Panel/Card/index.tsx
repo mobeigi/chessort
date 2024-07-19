4ec8e9bfff81eb906d078787863f7c423513c93d
Mo Beigi <me@mobeigi.com>
2024-07-19 15:19:53 +1000

    Add position icon and styling

--- app/src/App/Panel/Card/index.tsx
+++ app/src/App/Panel/Card/index.tsx
@@ -1,7 +1,8 @@
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
-import { CardContainer } from './styled';
+import { CardContainer, CurrentRankWrapper, CurrentRankIconWrapper, CurrentRankNumber, StatusIconWrapper, SanMoveWrapper } from './styled';
 import { CardProps } from './types';
+import CurrentRankIcon from '../../../assets/icons/two-way.svg?react';
 
 export const Card = ({ cardDetail }: CardProps) => {
     const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({ id: cardDetail.uciMove });
@@ -16,8 +17,16 @@ export const Card = ({ cardDetail }: CardProps) => {
         {...attributes}
         {...listeners}
       >
-        {cardDetail.curRank}&nbsp;
-        {cardDetail.sanMove}&nbsp;
+        <StatusIconWrapper>@</StatusIconWrapper>
+        <CurrentRankWrapper>
+          <CurrentRankIconWrapper>
+            <CurrentRankIcon />
+          </CurrentRankIconWrapper>
+          <CurrentRankNumber>{cardDetail.curRank}</CurrentRankNumber>
+        </CurrentRankWrapper>
+        <SanMoveWrapper>{cardDetail.sanMove}</SanMoveWrapper>
+        <span>C</span>
+        <span>EVAL</span>
       </CardContainer>
     );
   };
--- app/src/App/Panel/Card/styled.ts
+++ app/src/App/Panel/Card/styled.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+
 interface CardContainerProps {
   $isDragging: boolean;
 }
@@ -11,10 +12,45 @@ export const CardContainer = styled.div.attrs<CardContainerProps>(({ $isDragging
     position: $isDragging ? 'relative' : undefined,
   },
 }))<CardContainerProps>`
-  padding: 1em;
-  background-color: #000;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+  display: flex;
+  gap: 0.4em;
+  background-color: #b58b69;
+  color: #000;
+  padding: 1em 0.8em;
+  border: 2px solid #000;
+  border-radius: 0.2em;
   cursor: grab;
 `;
 
+export const CurrentRankWrapper = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 3px;
+`
+
+export const CurrentRankIconWrapper = styled.span`
+  display: flex;
+  
+  svg {
+    width: 16px;
+    height: 16px;
+  }
+`
+
+export const CurrentRankNumber = styled.span`
+  font-weight: bold;
+`
+
+export const StatusIconWrapper = styled.span`
+  display: block;
+  width: 24px;
+  height: 24px;
+`
+
+export const SanMoveWrapper = styled.span`
+  display: flex;
+  flex-grow: 1;
+  justify-content: center;
+  font-weight: bold;
+`
