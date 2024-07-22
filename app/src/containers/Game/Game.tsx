9705286485930cc1bbfadf204fc218959125dda4
Mo Beigi <me@mobeigi.com>
2024-07-22 23:17:40 +1000

    Refactor game structure

--- app/src/containers/Game/Game.tsx
+++ app/src/containers/Game/Game.tsx
@@ -5,26 +5,26 @@ import { GameProvider } from '../../context/gameContext';
 import { useGameContext } from '../../hooks/useGameContext';
 import { Square } from 'react-chessboard/dist/chessboard/types';
 
-const ChessBoardContainer = () => {
+const GameContent = () => {
   const { state } = useGameContext();
 
   const lastMoveFrom = state.previewedMove?.slice(0, 2) as Square;
   const lastMoveTo = state.previewedMove?.slice(2, 4) as Square;
 
-  return <ChessBoard fen={state.curChessJs.fen()} lastMoveFrom={lastMoveFrom} lastMoveTo={lastMoveTo} />;
-};
-
-export const Game = () => {
   return (
     <GameWrapper>
-      <GameProvider>
-        <ChessBoardWrapper>
-          <ChessBoardContainer />
-        </ChessBoardWrapper>
-        <PanelWrapper>
-          <Panel />
-        </PanelWrapper>
-      </GameProvider>
+      <ChessBoardWrapper>
+        <ChessBoard fen={state.curChessJs.fen()} lastMoveFrom={lastMoveFrom} lastMoveTo={lastMoveTo} />
+      </ChessBoardWrapper>
+      <PanelWrapper>
+        <Panel />
+      </PanelWrapper>
     </GameWrapper>
   );
 };
+
+export const Game = () => (
+  <GameProvider>
+    <GameContent />
+  </GameProvider>
+);
