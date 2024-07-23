f6fccc9d3e7abae90d695529fa4daae1609c3717
Mo Beigi <me@mobeigi.com>
2024-07-23 19:38:46 +1000

    Add media query to handle footer

--- app/src/containers/App/Footer/styled.ts
+++ app/src/containers/App/Footer/styled.ts
@@ -2,20 +2,30 @@ import styled from 'styled-components';
 
 export const FooterWrapper = styled.footer`
   display: flex;
-  min-height: 50px;
+  flex-direction: row;
 
+  margin-top: 1em;
   justify-content: center;
   align-items: center;
 
   font-size: 0.8em;
+
+  /* mobile */
+  @media (max-width: 800px) {
+    flex-direction: column;
+    gap: 0.5em;
+  }
 `;
 
 export const CreatedByWrapper = styled.div`
   display: flex;
+
+  /* Matches styling on board / panel. TODO: commonise this */
+  width: calc(min(65vw, 65vh));
+  min-width: 300px;
+
   justify-content: center;
   align-items: center;
-  flex: 0 0 calc(max(65vmin, 200px)); /* Matches styling on board / panel. TODO: commonise this */
-  display: flex;
   line-height: 1.2em;
 
   a {
@@ -33,7 +43,12 @@ export const PieceWrapper = styled.span`
 
 export const CopyrightDisclaimer = styled.div`
   display: flex;
-  flex: 0 0 calc(30vmin); /* Matches styling on board / panel. TODO: commonise this */
+
+  /* Matches styling on board / panel. TODO: commonise this */
+  width: calc(min(30vw, 30vh));
+  max-width: 100%; /* Avoid overflow */
+  min-width: calc((300px / 65) * 30);
+
   justify-content: center;
   align-items: center;
   font-size: 0.85em;
