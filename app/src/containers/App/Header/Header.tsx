e16d99c64c01840cf417e767b4166e2094aebba8
Mo Beigi <me@mobeigi.com>
2024-07-24 00:07:44 +1000

    Make header link to homepage on non-game pages

--- app/src/containers/App/Header/Header.tsx
+++ app/src/containers/App/Header/Header.tsx
@@ -5,8 +5,10 @@ import { useLoadGame } from '../../../hooks/useLoadGame';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { GameApiResponse } from '../../../services/chessortServer';
+import { useGameContext } from '../../../hooks/useGameContext';
 
 export const Header = () => {
+  const { state } = useGameContext();
   const navigate = useNavigate();
   const onLoadGameSuccess = useCallback(
     (game: GameApiResponse) => {
@@ -21,6 +23,13 @@ export const Header = () => {
   });
 
   const headerOnClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    // If no game loaded, this is not a game screen
+    // So we simply return to allow click to go through to target
+    if (!state.isInitialLoadCompleted) {
+      return;
+    }
+
+    // Otherwise, prevent click
     event.preventDefault();
 
     // Only load a new game if we're not already loading one
