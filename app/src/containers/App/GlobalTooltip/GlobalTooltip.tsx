de4693a8044b22ecea8bed472bf14b0b1fb08e1e
Mo Beigi <me@mobeigi.com>
2024-07-29 04:32:52 +1000

    Fix tooltip flickering when too wide, enforce max width/height for tooltips and make position fixed

--- app/src/containers/App/GlobalTooltip/GlobalTooltip.tsx
+++ app/src/containers/App/GlobalTooltip/GlobalTooltip.tsx
@@ -1,11 +1,10 @@
-import { Tooltip } from 'react-tooltip';
-import { GlobalTooltipWrapper } from './styled';
+import { GlobalTooltipWrapper, StyledTooltip } from './styled';
 
 // Provides a top level tooltip that can be reused by the entire app
 // It is best practice to store one copy of this tooltip and reuse it for performance and to avoid various DOM render bugs
+// TODO: Tooltip needs to support themes other than just dark
 export const GlobalTooltip = () => (
   <GlobalTooltipWrapper>
-    {/* TODO: this should support themes */}
-    <Tooltip id={`base-tooltip`} place="top" variant="dark" opacity={0.9} />
+    <StyledTooltip id={`base-tooltip`} place="top" variant="dark" opacity={0.9} />
   </GlobalTooltipWrapper>
 );
--- app/src/containers/App/GlobalTooltip/styled.ts
+++ app/src/containers/App/GlobalTooltip/styled.ts
@@ -1,7 +1,19 @@
 import { styled } from 'styled-components';
+import { Tooltip } from 'react-tooltip';
 import zIndex from '../../../styles/zindex';
 
 export const GlobalTooltipWrapper = styled.div`
   // Set desired z-index
   z-index: ${zIndex.tooltip};
+
+  // Make tooltips position fixed
+  // Has to be applied to wrapper div around tooltip
+  // This also fixes some flickering issues when focusing from one tooltip directly to another one that is wide on mobile
+  position: fixed;
+`;
+
+export const StyledTooltip = styled(Tooltip)`
+  // This ensures the tooltip contents are never bigger than the viewport itself
+  max-width: 90vw;
+  max-height: 90vh;
 `;
