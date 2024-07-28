fab1c2f486ce01d1cb3b4de470d185fee486253a
Mo Beigi <me@mobeigi.com>
2024-07-29 02:07:37 +1000

    Improve comment

--- app/src/containers/App/GlobalTooltip/GlobalTooltip.tsx
+++ app/src/containers/App/GlobalTooltip/GlobalTooltip.tsx
@@ -2,7 +2,7 @@ import { Tooltip } from 'react-tooltip';
 import { GlobalTooltipWrapper } from './styled';
 
 // Provides a top level tooltip that can be reused by the entire app
-// It is best practice to store one copy of this tooltip and reuse it for performance and to various DOM render bugs
+// It is best practice to store one copy of this tooltip and reuse it for performance and to avoid various DOM render bugs
 export const GlobalTooltip = () => (
   <GlobalTooltipWrapper>
     {/* TODO: this should support themes */}
