616cdcd9f0870282f6a5851fc69cb71be8190451
Mo Beigi <me@mobeigi.com>
2024-07-30 02:01:23 +1000

    Enforce a minimum width of 4 for sqids

--- server/chessort-server/utils.py
+++ server/chessort-server/utils.py
@@ -4,8 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Once set these should not change to avoid breaking existing hashes shared in the wild
 secret_alphabet = os.getenv('SQIDS_SECRET_ALPHABET', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-sqids = Sqids(alphabet=secret_alphabet)
+min_length = int(os.getenv('SQIDS_MIN_LENGTH', '2'))
+
+sqids = Sqids(alphabet=secret_alphabet, min_length=min_length)
 
 def generate_game_id(game_played_id):
     """ Generate a game ID using sqids """
