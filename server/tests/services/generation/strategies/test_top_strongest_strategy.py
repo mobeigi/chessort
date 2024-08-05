d586d4b41af0370f13615728397d0bd61795178d
Mo Beigi <me@mobeigi.com>
2024-08-06 03:29:29 +1000

    Use position fixture

--- server/tests/services/generation/strategies/test_top_strongest_strategy.py
+++ server/tests/services/generation/strategies/test_top_strongest_strategy.py
@@ -3,20 +3,20 @@ import pytest
 from chessortserver.services.generation.strategies.top_strongest_strategy import TopStrongestStrategy
 from chessortserver.models.models import Move
 
-from fixtures.fixtures import random_move
+from fixtures.fixtures import random_position, random_move
 
-position = '' # TODO: use fixture
+position = random_position()
 strategy_distinct = TopStrongestStrategy(distinct=True)
 strategy_not_distinct = TopStrongestStrategy(distinct=False)
 
 def test_distinct_can_handle_insufficient_moves_buckets():
     moves = [ 
-        Move(id=1, position_id=1, uci_move="uci1", engine_eval="#1", engine_overall_rank=1),
-        Move(id=2, position_id=1, uci_move="uci2", engine_eval="#1", engine_overall_rank=2),
-        Move(id=3, position_id=1, uci_move="uci3", engine_eval="#2", engine_overall_rank=3),
-        Move(id=4, position_id=1, uci_move="uci4", engine_eval="#2", engine_overall_rank=4),
-        Move(id=5, position_id=1, uci_move="uci5", engine_eval="#3", engine_overall_rank=5),
-        Move(id=6, position_id=1, uci_move="uci5", engine_eval="#3", engine_overall_rank=6),
+        Move(id=1, position_id=position.id, uci_move="uci1", engine_eval="#1", engine_overall_rank=1),
+        Move(id=2, position_id=position.id, uci_move="uci2", engine_eval="#1", engine_overall_rank=2),
+        Move(id=3, position_id=position.id, uci_move="uci3", engine_eval="#2", engine_overall_rank=3),
+        Move(id=4, position_id=position.id, uci_move="uci4", engine_eval="#2", engine_overall_rank=4),
+        Move(id=5, position_id=position.id, uci_move="uci5", engine_eval="#3", engine_overall_rank=5),
+        Move(id=6, position_id=position.id, uci_move="uci5", engine_eval="#3", engine_overall_rank=6),
      ]
     assert len(moves) == 6
     
@@ -56,16 +56,16 @@ def _test_helper(strategy: TopStrongestStrategy, moves: list[Move], num_required
 
 def test_top_strongest_strategy():
     moves = [ 
-        Move(id=1, position_id=1, uci_move="uci1", engine_eval="#1", engine_overall_rank=1),
-        Move(id=2, position_id=1, uci_move="uci2", engine_eval="#2", engine_overall_rank=2),
-        Move(id=3, position_id=1, uci_move="uci3", engine_eval="+200", engine_overall_rank=3),
-        Move(id=4, position_id=1, uci_move="uci4", engine_eval="+200", engine_overall_rank=4),
-        Move(id=5, position_id=1, uci_move="uci5", engine_eval="0", engine_overall_rank=5),
-        Move(id=6, position_id=1, uci_move="uci6", engine_eval="0", engine_overall_rank=6),
-        Move(id=7, position_id=1, uci_move="uci7", engine_eval="0", engine_overall_rank=7),
-        Move(id=8, position_id=1, uci_move="uci8", engine_eval="-16", engine_overall_rank=8),
-        Move(id=9, position_id=1, uci_move="uci9", engine_eval="-16", engine_overall_rank=9),
-        Move(id=10, position_id=1, uci_move="uci10", engine_eval="#-5", engine_overall_rank=10),
+        Move(id=1, position_id=position.id, uci_move="uci1", engine_eval="#1", engine_overall_rank=1),
+        Move(id=2, position_id=position.id, uci_move="uci2", engine_eval="#2", engine_overall_rank=2),
+        Move(id=3, position_id=position.id, uci_move="uci3", engine_eval="+200", engine_overall_rank=3),
+        Move(id=4, position_id=position.id, uci_move="uci4", engine_eval="+200", engine_overall_rank=4),
+        Move(id=5, position_id=position.id, uci_move="uci5", engine_eval="0", engine_overall_rank=5),
+        Move(id=6, position_id=position.id, uci_move="uci6", engine_eval="0", engine_overall_rank=6),
+        Move(id=7, position_id=position.id, uci_move="uci7", engine_eval="0", engine_overall_rank=7),
+        Move(id=8, position_id=position.id, uci_move="uci8", engine_eval="-16", engine_overall_rank=8),
+        Move(id=9, position_id=position.id, uci_move="uci9", engine_eval="-16", engine_overall_rank=9),
+        Move(id=10, position_id=position.id, uci_move="uci10", engine_eval="#-5", engine_overall_rank=10),
      ]
     assert len(moves) == 10
 
