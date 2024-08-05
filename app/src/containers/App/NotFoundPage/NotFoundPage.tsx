aba24b33643d7f4e26c9ada89eb2e1bd7e95417c
Mo Beigi <me@mobeigi.com>
2024-08-06 02:58:01 +1000

    Title for 404 page

--- app/src/containers/App/NotFoundPage/NotFoundPage.tsx
+++ app/src/containers/App/NotFoundPage/NotFoundPage.tsx
@@ -1,14 +1,22 @@
 import { Link } from 'react-router-dom';
 import { NotFoundContainer } from './styled';
+import { Helmet } from 'react-helmet-async';
+import { APP_NAME, CHESSORT_APP_BASE_URL } from '../../../constants/app';
 
 export const NotFoundPage = () => {
   return (
-    <NotFoundContainer>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for could not be found.</p>
-      <p>
-        <Link to="/">Visit homepage</Link>
-      </p>
-    </NotFoundContainer>
+    <>
+      <Helmet>
+        <title>{APP_NAME}: 404 Not Found</title>
+        <link rel="canonical" href={`${CHESSORT_APP_BASE_URL}/404`} />
+      </Helmet>
+      <NotFoundContainer>
+        <h1>404 Not Found</h1>
+        <p>The page you are looking for could not be found.</p>
+        <p>
+          <Link to="/">Visit homepage</Link>
+        </p>
+      </NotFoundContainer>
+    </>
   );
 };
