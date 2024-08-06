a11db0e88a5fc967e9e14ab986353c57813b381f
Mo Beigi <me@mobeigi.com>
2024-08-06 18:25:08 +1000

    Preserve shape state per preview mode

--- app/src/components/ChessgroundBoard/ChessgroundBoard.tsx
+++ app/src/components/ChessgroundBoard/ChessgroundBoard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useImperativeHandle, forwardRef } from 'react';
 import { ChessgroundContainer } from './styled';
 import { Chessground } from 'chessground';
 import { Api } from 'chessground/api';
@@ -9,36 +9,42 @@ import '../../assets/chessground/chessground.base.css';
 import '../../assets/pieces/maestro/chessground.maestro.external.css';
 import '../../assets/chessground/chessground.chessort.css';
 
-export const ChessgroundBoard = ({
-  fen,
-  lastMove = [],
-  turnColor = 'white',
-  orientation = 'white',
-}: ChessGroundBoardProps) => {
-  const boardRef = useRef(null);
-  const [api, setApi] = useState<Api>();
+export const ChessgroundBoard = forwardRef<Api | undefined, ChessGroundBoardProps>(
+  ({ fen, lastMove = [], turnColor = 'white', orientation = 'white' }: ChessGroundBoardProps, apiRef) => {
+    const boardRef = useRef(null);
+    const [api, setApi] = useState<Api>();
 
-  // Init the board on first render
-  useEffect(() => {
-    if (boardRef.current) {
-      const api = Chessground(boardRef.current, initialConfig);
-      setApi(api);
-      return () => {
-        api.destroy();
-      };
-    }
-  }, []);
+    // Init the board on first render
+    useEffect(() => {
+      if (boardRef.current) {
+        const api = Chessground(boardRef.current, initialConfig);
+        setApi(api);
+        return () => {
+          api.destroy();
+        };
+      }
+    }, []);
 
-  // Update board state via API
-  // This essentially binds the props to the API
-  useEffect(() => {
-    api?.set({
-      fen,
-      lastMove,
-      turnColor,
-      orientation,
-    });
-  }, [api, fen, lastMove, orientation, turnColor]);
+    // Update board state via API
+    // This essentially binds the props to the API
+    useEffect(() => {
+      if (api) {
+        console.log(api.state.drawable.shapes);
+        api.set({
+          fen,
+          lastMove,
+          turnColor,
+          orientation,
+          drawable: {
+            ...api.state.drawable,
+          },
+        });
+      }
+    }, [api, fen, lastMove, turnColor, orientation]);
 
-  return <ChessgroundContainer ref={boardRef}></ChessgroundContainer>;
-};
+    // Expose API ref to parent
+    useImperativeHandle(apiRef, () => api, [api]);
+
+    return <ChessgroundContainer ref={boardRef}></ChessgroundContainer>;
+  },
+);
--- app/src/containers/Game/Game.tsx
+++ app/src/containers/Game/Game.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { GameWrapper, ChessBoardWrapper, PanelWrapper } from './styled';
 import ChessGroundBoard from '../../components/ChessgroundBoard';
 import Panel from '../Panel';
@@ -6,6 +7,8 @@ import { Key, Color } from 'chessground/types';
 import useUserPreferences from '../../hooks/useUserPreferences';
 import { BoardOrientation } from '../../context/userPreferencesContext';
 import { Color as ChessJsColor } from 'chess.js';
+import { Api } from 'chessground/api';
+import { DrawShape } from 'chessground/draw';
 
 const chessJsTurnToColor = (turnColor: ChessJsColor): Color => {
   return turnColor === 'w' ? 'white' : 'black';
@@ -31,6 +34,77 @@ export const Game = () => {
   const { state } = useGameContext();
   const { boardOrientation } = useUserPreferences();
 
+  const stateRef = useRef<typeof state | null>(state);
+  const apiRef = useRef<Api>();
+
+  const [lastPreviewedMove, setLastPreviewedMove] = useState<string | null>(state.previewedMove);
+  const [shapes, setShapes] = useState<Map<string | null, DrawShape[]>>(new Map());
+
+  /**
+   * Store a ref to our state for callbacks.
+   */
+  useEffect(() => {
+    stateRef.current = state;
+  }, [state]);
+
+  /**
+   * Reset shapes on Fen change.
+   */
+  useEffect(() => {
+    if (apiRef.current) {
+      setLastPreviewedMove(state.previewedMove);
+      setShapes(new Map());
+      apiRef.current.setShapes([]);
+      apiRef.current.redrawAll();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [apiRef.current, state.gameDetails.fen]);
+
+  /**
+   * Called when shapes are updated via user interaction.
+   */
+  const onShapesChanged = useCallback((shapes: DrawShape[]) => {
+    if (stateRef.current) {
+      const previewedMove = stateRef.current.previewedMove;
+      setShapes((prevShapes) => {
+        const updatedShapes = new Map(prevShapes);
+        updatedShapes.set(previewedMove, shapes);
+        return updatedShapes;
+      });
+    }
+  }, []);
+
+  /**
+   * Set shapes onChange handler once as api ref becomes available.
+   */
+  useEffect(() => {
+    if (apiRef.current) {
+      apiRef.current.state.drawable.onChange = onShapesChanged;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [apiRef.current]);
+
+  /**
+   * Load stored shapes as previewed move changes.
+   */
+  useEffect(() => {
+    if (apiRef.current) {
+      if (state.previewedMove !== lastPreviewedMove) {
+        if (shapes) {
+          const storedShapes = shapes.get(state.previewedMove);
+          if (storedShapes) {
+            apiRef.current.setShapes(storedShapes);
+          } else {
+            apiRef.current.setShapes([]);
+          }
+          apiRef.current.redrawAll();
+        }
+        setLastPreviewedMove(state.previewedMove);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [apiRef.current, state.previewedMove, lastPreviewedMove]);
+
   const lastMoveFrom = state.previewedMove?.slice(0, 2) as Key;
   const lastMoveTo = state.previewedMove?.slice(2, 4) as Key;
   const lastMove = [...(lastMoveFrom && lastMoveTo ? [lastMoveFrom, lastMoveTo] : [])];
@@ -42,6 +116,7 @@ export const Game = () => {
     <GameWrapper>
       <ChessBoardWrapper>
         <ChessGroundBoard
+          ref={apiRef}
           fen={state.curChessJs.fen()}
           turnColor={chessJsTurnToColor(state.initialChessJs.turn())}
           lastMove={lastMove}
